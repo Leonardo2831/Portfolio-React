70f662ba99c26de165e50e26dcfacec4b92eb2db
Leonardo2831 <leoferraz1818@gmail.com>
2026-05-19 21:41:47 -0300

    new prop to circle pulse

--- src/components/layout/header/TitleName.jsx
+++ src/components/layout/header/TitleName.jsx
@@ -3,7 +3,7 @@ import CirclePulse from "../../ui/CirclePulse";
 export default function TitleName(){
     return (
         <div className="flex items-center gap-2.5">
-            <CirclePulse />
+            <CirclePulse color="crimson" />
             <h1 className="text-sm font-medium *:text-sm">
                 <span className="text-(--dim)">leonardo</span>
                 <span className="text-(--foreground) font-bold">.dev</span>

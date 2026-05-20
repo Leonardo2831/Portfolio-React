263c61d59db6b3f8fe3edd9bb2d60feca4756502
Leonardo2831 <leoferraz1818@gmail.com>
2026-05-19 22:28:45 -0300

    new style to image button

--- src/components/layout/header/Header.jsx
+++ src/components/layout/header/Header.jsx
@@ -6,15 +6,29 @@ import MenuMobile from "./MenuMobile";
 import React from "react";
 
 export default function Header() {
-    const src = 'public/icons/light-theme.svg';
-    const alt = 'Tema escuro';
-    const classStyle = 'w-4 h-4';
-
     const [isMobileOpen, setIsMobileOpen] = React.useState(false);
 
     const toggleMobileMenu = () => {
         setIsMobileOpen(!isMobileOpen);
     };
+
+    const [isTheme, setIsTheme] = React.useState(() => {
+        const savedTheme = localStorage.getItem('theme');
+        return savedTheme === 'dark';
+    });
+
+    React.useEffect(() => {
+        document.body.classList.toggle('dark', isTheme);
+    }, [isTheme]);
+
+    const toggleTheme = () => {
+        setIsTheme(!isTheme);
+        localStorage.setItem('theme', !isTheme ? 'dark' : 'light');
+    };
+
+    const src = isTheme ? '/icons/dark-theme.svg' : '/icons/light-theme.svg';
+    const alt = isTheme ? 'Tema escuro' : 'Tema claro';
+    const classStyle = 'w-4 h-4';
 
     return (
         <div className="bg-(--background) text-(--foreground)">
@@ -24,7 +38,7 @@ export default function Header() {
                 <div className="flex items-center justify-center flex-wrap gap-2">
                     <ButtonConfig id="pt" name="language" type="radio" children="PT" defaultChecked />
                     <ButtonConfig id="en" name="language" type="radio" children="EN" />
-                    <ButtonConfig id="dark" name="theme" type="checkbox" children={<Img src={src} alt={alt} classStyle={classStyle} />} />
+                    <ButtonConfig onClick={toggleTheme} id="dark" name="theme" type="checkbox" children={<Img src={src} alt={alt} classStyle={classStyle} />} />
                     <ButtonConfig onClick={toggleMobileMenu} id="menu" name="menu" type="checkbox" children={<MenuMobile />} visible="md:hidden" />
                 </div>
             </header>

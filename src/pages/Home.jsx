fce3b1d0aea6cd52fc33a08879faad7bf69681c5
Leonardo2831 <leoferraz1818@gmail.com>
2026-05-20 16:01:23 -0300

    logic to theme

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,31 @@
+import React from "react";
 import Header from "../components/layout/header/Header";
 import Introduction from "../components/layout/introduction/Introduction";
+import Section from "../components/ui/Section";
+import Sobre from "../components/layout/Sobre";
 
 export default function Home(){
+    const [isTheme, setIsTheme] = React.useState(() => {
+        const savedTheme = localStorage.getItem('theme');
+        return savedTheme === 'dark';
+    });
+
+    React.useEffect(() => {
+        document.body.classList.toggle('dark', isTheme);
+    }, [isTheme]);
+
+    const toggleTheme = () => {
+        setIsTheme(!isTheme);
+        localStorage.setItem('theme', !isTheme ? 'dark' : 'light');
+    };
+
     return (
         <>
-            <Header />
-            <Introduction />
+            <Header isTheme={isTheme} toggleTheme={toggleTheme} />
+            <Introduction isTheme={isTheme} />
+            <Section idSection="sobre" number={1} title="Sobre mim" code="get-content about.md" >
+                <Sobre />
+            </Section>
         </>
     );  
 }

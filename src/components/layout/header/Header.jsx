4d7d602bbf6a6f835f4f4566d52010d476d304fe
Leonardo2831 <leoferraz1818@gmail.com>
2026-05-19 21:41:35 -0300

    remove log and new style responsive

--- src/components/layout/header/Header.jsx
+++ src/components/layout/header/Header.jsx
@@ -13,20 +13,19 @@ export default function Header() {
     const [isMobileOpen, setIsMobileOpen] = React.useState(false);
 
     const toggleMobileMenu = () => {
-        console.log("Abrindo/fechando menu. Novo estado:", !isMobileOpen);
         setIsMobileOpen(!isMobileOpen);
     };
 
     return (
         <div className="bg-(--background) text-(--foreground)">
-            <header className="relative px-10 py-4 max-w-7xl mx-auto flex items-center justify-between">
+            <header className="relative px-6 sm:px-10 py-3 sm:py-4 max-w-7xl mx-auto flex items-center justify-between gap-5">
                 <TitleName />
                 <Nav classMenu={`md:block ${isMobileOpen ? 'block' : 'hidden'}`} />
-                <div className="flex items-center gap-2">
+                <div className="flex items-center justify-center flex-wrap gap-2">
                     <ButtonConfig id="pt" name="language" type="radio" children="PT" defaultChecked />
                     <ButtonConfig id="en" name="language" type="radio" children="EN" />
                     <ButtonConfig id="dark" name="theme" type="checkbox" children={<Img src={src} alt={alt} classStyle={classStyle} />} />
-                    <ButtonConfig onClick={toggleMobileMenu} id="menu" name="menu" type="checkbox" children={<MenuMobile />} />
+                    <ButtonConfig onClick={toggleMobileMenu} id="menu" name="menu" type="checkbox" children={<MenuMobile />} visible="md:hidden" />
                 </div>
             </header>
         </div>

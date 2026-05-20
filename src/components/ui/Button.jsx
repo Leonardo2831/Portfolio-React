5f52a87f85494afeb13c4e59f48790584934b8bf
Leonardo2831 <leoferraz1818@gmail.com>
2026-05-20 14:26:38 -0300

    button component

---
+++ src/components/ui/Button.jsx
@@ -0,0 +1,15 @@
+export default function Button(props){
+    const styleButton = {
+        'red': 'bg-accent-crimson text-(--background) hover:bg-accent-salmon transition-colors',
+        'black': 'bg-(--background-elevated) border border-(--border-gray) text-(--foreground) hover:border-(--foreground) transition-colors'
+    }
+
+    const classButton = `${styleButton[props.color]} text-xs flex items-center gap-2.5 rounded-lg py-4 px-6 cursor-pointer`
+
+    return (
+        <button className={classButton}>
+            {props.text}
+            {props.children}
+        </button>
+    );
+}

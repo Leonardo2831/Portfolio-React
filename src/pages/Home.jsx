75c71a34a634b7bd8207b41c77185e384fbe1eef
Leonardo2831 <leoferraz1818@gmail.com>
2026-05-19 22:40:47 -0300

    new item in home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,11 @@
 import Header from "../components/layout/header/Header";
+import Introduction from "../components/layout/introduction/Introduction";
 
 export default function Home(){
     return (
-        <Header />
-    );
+        <>
+            <Header />
+            <Introduction />
+        </>
+    );  
 }

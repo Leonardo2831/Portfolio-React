4943c56a78bb7878776b33e1a583a8de69c3716f
Leonardo2831 <leoferraz1818@gmail.com>
2026-05-20 14:26:31 -0300

    new title apresentation

---
+++ src/components/layout/introduction/TitleApresentation.jsx
@@ -0,0 +1,9 @@
+export default function TitleApresentation() {
+    return (
+        <h1 className="space-y-[0.5px]">
+            <p className="font-sans font-light text-8xl text-(--foreground) tracking-[-3.6px]">Leonardo Reis Ferraz</p>
+            <p className="font-sans font-light text-(--dim) text-7xl tracking-[-3.6px]">Desenvolvedor</p>
+            <p className="font-serif italic text-accent-crimson text-8xl tracking-[-3.6px] lowercase">Front-End</p>
+        </h1>
+    );
+}

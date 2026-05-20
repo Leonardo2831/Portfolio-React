27230ff091fed4708609b291a787aea2272a7bd3
Leonardo2831 <leoferraz1818@gmail.com>
2026-05-20 20:15:39 -0300

    new style font button

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -2,9 +2,9 @@ export default function Button(props){
     const styleButton = {
         'red': 'bg-accent-crimson text-(--background) hover:bg-accent-salmon transition-colors',
         'black': 'bg-(--background-elevated) border border-(--border-gray) text-(--foreground) hover:border-(--foreground) transition-colors'
-    }
+    };
 
-    const classButton = `${styleButton[props.color]} text-xs flex items-center gap-2.5 rounded-lg py-4 px-6 cursor-pointer`
+    const classButton = `${styleButton[props.color]} text-xs font-medium flex items-center gap-2.5 rounded-lg py-4 px-6 cursor-pointer`;
 
     return (
         <button className={classButton}>

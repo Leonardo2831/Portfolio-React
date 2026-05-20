b2430f3144f43d5b2db51fa7eb13136c8229f6fe
Leonardo2831 <leoferraz1818@gmail.com>
2026-05-20 16:00:50 -0300

    new title apresentation to introduction

--- src/components/layout/introduction/TitleApresentation.jsx
+++ src/components/layout/introduction/TitleApresentation.jsx
@@ -1,9 +1,9 @@
 export default function TitleApresentation() {
     return (
-        <h1 className="space-y-[0.5px]">
-            <p className="font-sans font-light text-8xl text-(--foreground) tracking-[-3.6px]">Leonardo Reis Ferraz</p>
-            <p className="font-sans font-light text-(--dim) text-7xl tracking-[-3.6px]">Desenvolvedor</p>
-            <p className="font-serif italic text-accent-crimson text-8xl tracking-[-3.6px] lowercase">Front-End</p>
+        <h1 className="flex flex-col sm:gap-1">
+            <span className="font-sans font-light text-5xl xs:text-7xl lg:text-8xl text-(--foreground) tracking-[-3.6px] leading-[0.95] sm:leading-[0.9]">Leonardo Reis Ferraz</span>
+            <span className="font-sans font-light text-(--dim) text-4xl xs:text-6xl lg:text-7xl tracking-[-3.6px] leading-[0.95] sm:leading-[0.9]">Desenvolvedor</span>
+            <span className="font-serif italic text-accent-crimson text-5xl xs:text-7xl lg:text-8xl tracking-[-3.6px] leading-[0.95] sm:leading-[0.9] lowercase">Front-End</span>
         </h1>
     );
 }

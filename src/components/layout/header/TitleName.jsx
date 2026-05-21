9e1b8205960e669d054a0889261daafff3f4a48a
Leonardo2831 <leoferraz1818@gmail.com>
2026-05-21 16:56:46 -0300

    change import

--- src/components/layout/header/TitleName.jsx
+++ src/components/layout/header/TitleName.jsx
@@ -1,9 +1,9 @@
-import CirclePulse from "../../ui/CirclePulse";
+import ItemPulse from "../../ui/ItemPulse";
 
 export default function TitleName(){
     return (
         <div className="flex items-center gap-2.5">
-            <CirclePulse color="crimson" />
+            <ItemPulse color="crimson" />
             <h1 className="text-sm font-medium *:text-sm">
                 <span className="text-(--dim)">leonardo</span>
                 <span className="text-(--foreground) font-bold">.dev</span>

1bb6bd2267455b3013de9383fdc3ff326b0c7a15
Leonardo2831 <leoferraz1818@gmail.com>
2026-05-20 22:33:18 -0300

    new style tag item

---
+++ src/components/layout/about/TagMark.jsx
@@ -0,0 +1,5 @@
+export default function TagMark({ text }) {
+    return (
+        <span className="px-2 py-0.75 bg-(--background-elevated) border border-(--border-gray) text-lg font-mono font-normal rounded-sm">{text.toString().trim()}</span>
+    );
+}
